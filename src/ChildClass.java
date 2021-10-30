259fe8b277dab5717295ea14cfd7ff69cb5654f9
haeky <haeky@DESKTOP-GQH4R2D>
2021-10-30 16:57:45 +0900

    Inheritance Exercise

---
+++ src/ChildClass.java
@@ -0,0 +1,10 @@
+
+public class ChildClass extends ParentClass {
+	public ChildClass() {
+		System.out.println("ChildClass constructor");
+	}
+
+	public void childFun() {
+		System.out.println("childFun()");
+	}
+}
---
+++ src/MainClass.java
@@ -0,0 +1,10 @@
+
+public class MainClass {
+	public static void main(String[] arg) {
+		ChildClass childClass = new ChildClass();
+		childClass.childFun();
+		childClass.parentFun();
+		
+		//childClass.privateFun();
+	}
+}
